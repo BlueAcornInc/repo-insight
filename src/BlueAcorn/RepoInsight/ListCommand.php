c843d4327633bd8d8c98516c91e28586c97c31b2
Brice Burgess <nesta@iceburg.net>
2015-03-28 17:23:01 -0400

    import blueacorn/beanstalkapp-api

--- src/BlueAcorn/RepoInsight/ListCommand.php
+++ src/BlueAcorn/RepoInsight/ListCommand.php
@@ -5,6 +5,10 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use BlueAcorn\BeanstalkAppApi\BeanstalkAppAPI;
+use BlueAcorn\BeanstalkAppApi\BeanstalkAppAPIException;
+
+
 class ListCommand extends Command
 {
 
@@ -15,6 +19,8 @@ class ListCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+
+        $x = new BeanstalkAppAPI();
         $output->writeln('.... repo list test ....');
     }
 }
