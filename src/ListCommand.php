97c42672b53a1bc8eee9e6141e1c14c36f91876d
Brice Burgess <nesta@iceburg.net>
2015-03-30 13:41:07 -0400

    fix namespacing

--- src/ListCommand.php
+++ src/ListCommand.php
@@ -5,8 +5,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use BlueAcorn\BeanstalkAppApi\BeanstalkAppAPI;
-use BlueAcorn\BeanstalkAppApi\BeanstalkAppAPIException;
+use BlueAcorn\beanstalkapp\BeanstalkAppAPI;
 
 
 class ListCommand extends Command
