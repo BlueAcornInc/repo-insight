5b56c486d1955bd5c56f01b7e885b24a50f7503d
Brice Burgess <nesta@iceburg.net>
2015-04-01 18:18:20 -0400

    include attask library

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,8 @@
 <?php
-
-
 namespace BlueAcorn\RepoInsight;
+
+// require attask
+require_once(__DIR__ . '/../lib/attask/src/StreamClient.php');
 
 $application = new Application('RepoInsight','@package_version@');
 
